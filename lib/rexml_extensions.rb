70cf27726234104a13225e7f51396efa42299718
Alexander Simonov <alex@simonov.in.ua>
2009-09-16 15:40:39 +0300

    making links and twitter user name to links

--- lib/rexml_extensions.rb
+++ lib/rexml_extensions.rb
@@ -6,7 +6,14 @@ class REXML::Element
   protected
     def convert_node_to_hash(node, hash)
       node.elements.each do |elm|
-        hash[elm.name] = elm.elements.empty? ? elm.text : convert_node_to_hash(elm, hash)
+        hash[elm.name] = if elm.elements.empty? then 
+            elm.text.to_s.gsub(/(^|[\n ])([\w]+?:\/\/[\w]+[^ \"\n\r\t<]*)/mi,
+                               '\1<a href="\2">\2</a>').gsub(
+                               /(\@(\w+))/,
+                               "<a href=\"http://twitter.com/\\2\">\\1</a>") 
+          else
+            convert_node_to_hash(elm, hash)
+          end
       end
       return hash
     end
